d854632d0d467a7749c90036fb084a5b22efdc1b
Balaji Amara <balajiamara2004@gmail.com>
2025-12-14 13:15:26 +0530

    Fix ASGI setup order for Channels

--- Backend/chat_pro/chat_pro/asgi.py
+++ Backend/chat_pro/chat_pro/asgi.py
@@ -12,11 +12,13 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat_app.routing
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_pro.settings")
 
 django.setup()
+
+import chat_app.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
